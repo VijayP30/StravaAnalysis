d4d854cdce196b0da80ba8edb32e62a48c64b663
pvijay03 <pvijay@ucsd.edu>
2023-09-06 22:13:09 -0700

    update csv

--- app.py
+++ app.py
@@ -6,10 +6,8 @@ from folium.plugins import HeatMap
 import pandas as pd
 import polyline
 from flask import Flask, render_template
-# from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-# bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
